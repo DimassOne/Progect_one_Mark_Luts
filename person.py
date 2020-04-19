da45eaece7c3d40c13d71f4c6dd86168348b6cd0
DimassOne <dimarru1@rambler.ru>
2020-04-19 10:20:35 +0300

    Phoo commit

--- person.py
+++ person.py
@@ -1,5 +1,19 @@
 class Person:
-    def __init__(self, name, job, pay):
+    def __init__(self, name, job=None, pay=0):
         self.name = name
         self.job = job
         self.pay = pay
+
+
+if __name__ == '__main__':
+    bob = Person('Bobbi')
+    jim = Person('Jimmi','Svarnoy',45000)
+    print(bob.name, bob.job, bob.pay)
+    print(jim.name, jim.job, jim.pay)
+    jim.pay *=1.3
+    print('%.3f' % jim.pay)
+
+
+
+
+
