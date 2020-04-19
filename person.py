dc4e05e854bc72d70fbf41f2bb66e1026bb929b0
DimassOne <dimarru1@rambler.ru>
2020-04-19 17:40:51 +0300

    Eigth commit

--- person.py
+++ person.py
@@ -1,3 +1,4 @@
+ЗАКОНЧИЛ КНИГУ НА 71 СТРАНИЦЕ
 class Person:
     def __init__(self, name, job=None, pay=0):
         self.name = name
@@ -9,7 +10,12 @@ class Person:
         self.pay = int(self.pay*(1+percent))
     def __repr__(self):
         return f"[Person: {self.name}, Зарплата: {self.pay}]"#'[Person: %s, %s]' % (self.name, self.pay)
-
+    
+class Manager(Person):
+    def __init__(self, name, pay):
+        Person.__init__(self, name, 'mgr', pay)
+    def gitveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent+bonus)
 
 if __name__ == '__main__':
     bob = Person('Dmitrii Rogonian')
@@ -19,6 +25,14 @@ if __name__ == '__main__':
     print(bob.lastName(), sue.lastName())
     sue.giveRaise(.10)
     print(sue)
+    tom = Manager('Tom Jones',50000)
+    tom.gitveRaise(.10)
+    print(tom.lastName())
+    print(tom)
+    print('--Полное дерево--')
+    for obj in (bob, sue, tom):
+        obj.giveRaise(.10)
+        print(obj)
 
 
 
